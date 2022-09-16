3cc48defbef034c5aebc90c28053fd149a644048
PolinaShchetsina <pollimolli210@gmail.com>
2022-09-16 22:07:15 +0300

    feat: add laboratory works

---
+++ semester 3/L-LPaT/laboratory_work_1/laboratory work 1/laboratory work 1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+#include <regex>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	string str;
+	cout << "Enter string: ";
+	cin >> str;
+
+	regex reg("ab*((c|d)*e)*");
+
+	if (regex_match(str, reg)) {
+		cout << "Result: YES" << endl;
+	}
+	else {
+		cout << "Result: NO" << endl;
+	}
+	return 0;
+}
